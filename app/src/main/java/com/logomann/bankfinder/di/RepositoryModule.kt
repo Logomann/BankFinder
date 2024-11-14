a1342160e804a86cb9f2b62b8a843f0ef9589fe8
Vitaliy Molodanov <53826004+Logomann@users.noreply.github.com>
2024-11-15 01:16:48 +0300

    Добавлена логика, экраны, подключены библиотеки

--- app/src/main/java/com/logomann/bankfinder/di/RepositoryModule.kt
+++ app/src/main/java/com/logomann/bankfinder/di/RepositoryModule.kt
@@ -6,8 +6,6 @@ import com.logomann.bankfinder.data.external.impl.ExternalNavigatorImpl
 import com.logomann.bankfinder.domain.card.CardRepository
 import com.logomann.bankfinder.domain.external.ExternalNavigator
 import org.koin.dsl.module
-import org.koin.dsl.single
-import kotlin.math.sin
 
 val repositoryModule = module {
 
